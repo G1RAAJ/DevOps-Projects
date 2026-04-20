8031209d8c4073401dda467aca77f6fedae63d51
G1RAAJ <jeevanraju.ch@gmail.com>
2026-04-20 22:00:04 +0530

    Update eks.tf

--- Nginx_eks/Terraform/eks.tf
+++ Nginx_eks/Terraform/eks.tf
@@ -8,12 +8,12 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  # ✅ FIX: Enable public access
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
-
-  # ✅ For practice (allow all)
   cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
+
+  # 🔥 IMPORTANT FIX
+  enable_cluster_creator_admin_permissions = true
 
   eks_managed_node_groups = {
     default = {
@@ -21,11 +21,8 @@ module "eks" {
       max_size     = 2
       min_size     = 1
 
-      # ✅ FIX: use supported instance
       instance_types = ["t3.micro"]
-
-      # Optional but safe
-      ami_type = "AL2_x86_64"
+      ami_type       = "AL2_x86_64"
     }
   }
 }
