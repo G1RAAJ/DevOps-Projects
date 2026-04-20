d537d471bd8d44ac1cd85cbae82a444a61e3495c
G1RAAJ <jeevanraju.ch@gmail.com>
2026-04-20 17:42:21 +0530

    Update eks.tf

--- Nginx_eks/Terraform/eks.tf
+++ Nginx_eks/Terraform/eks.tf
@@ -10,8 +10,8 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
-      desired_size = 2
-      max_size     = 3
+      desired_size = 1
+      max_size     = 2
       min_size     = 1
 
       instance_types = ["m7i-flex.large"]
