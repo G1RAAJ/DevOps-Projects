67ab3b64136a9ce90bf3e0e0853e0b261747cdbc
G1RAAJ <jeevanraju.ch@gmail.com>
2026-05-01 17:59:15 +0530

    Update main.tf

--- nginx-myeks/Tform/main.tf
+++ nginx-myeks/Tform/main.tf
@@ -2,52 +2,40 @@ provider "aws" {
   region = "us-east-1"
 }
 
-########################################
-# EKS CLUSTER
-########################################
-
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "20.37.2"
-
+  source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "myeks-1"
   cluster_version = "1.30"
 
-  vpc_id = "vpc-09a23ce107252dc4b"
+  vpc_id     = "vpc-09a23ce107252dc4b"
+  subnet_ids = ["subnet-04a75877b20cf9bf4 "subnet-079b66fb0be7efc65"]
 
-  subnet_ids = [
-    "subnet-04a75877b20cf9bf4",
-    "subnet-079b66fb0be7efc65"
-  ]
+  enable_irsa = true
 
-  ########################################
-  # 🔥 NETWORK FIX (IMPORTANT)
-  ########################################
-  cluster_endpoint_public_access       = true
-  cluster_endpoint_private_access      = false
-  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
-
-  ########################################
-  # 🔥 FIX: avoid CloudWatch conflict
-  ########################################
-  create_cloudwatch_log_group = false
-
-  ########################################
-  # NODE GROUP
-  ########################################
   eks_managed_node_groups = {
     default = {
-      instance_types = ["t3.small"]
-
       desired_size = 2
+      max_size     = 2
       min_size     = 1
-      max_size     = 3
-
-      capacity_type = "ON_DEMAND"
+      instance_types = ["t2.medium"]
     }
   }
+}
 
-  tags = {
-    Project = "nginx-eks"
+# 🔥 IMPORTANT: aws-auth mapping
+resource "kubernetes_config_map_v1" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapRoles = yamlencode([
+      {
+        rolearn  = "arn:aws:iam::652942059461:role/myeks-1-role"
+        username = "jenkins"
+        groups   = ["system:masters"]
+      }
+    ])
   }
 }
